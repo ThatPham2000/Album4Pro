a2922cab4b633750ad09f91fafc544a3ca71d829
Phạm Văn Thật <thatpham2000@gmail.com>
2021-10-21 17:02:43 +0700

    fix action bar

--- app/src/main/java/com/example/album4pro/MainActivity.java
+++ app/src/main/java/com/example/album4pro/MainActivity.java
@@ -102,16 +102,40 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "Show Camera", Toast.LENGTH_SHORT).show();
                 return true;
 
-            case R.id.action_option1:
+            case R.id.action_slideshow:
                 // User chose the "Favorite" action, mark the current item
                 // as a favorite...
-                Toast.makeText(this, "Show option 1", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Show slide show", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_sort_image:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                Toast.makeText(this, "Show sort image", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_sort_video:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                Toast.makeText(this, "Show sort video", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_load_url:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                Toast.makeText(this, "Show load ảnh từ url", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_selection:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                Toast.makeText(this, "Show selection", Toast.LENGTH_SHORT).show();
                 return true;
 
             default:
                 // If we got here, the user's action was not recognized.
                 // Invoke the superclass to handle it.
-                Toast.makeText(this, "Show Default", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(this, "Show Default", Toast.LENGTH_SHORT).show();
                 return super.onOptionsItemSelected(item);
 
         }
