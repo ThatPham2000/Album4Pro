8dcf67737da160377511a2a88b5b30d84031ade5
Phạm Văn Thật <thatpham2000@gmail.com>
2021-11-07 11:39:38 +0700

    share image

--- app/src/main/java/com/example/album4pro/gallery/DetailPhoto.java
+++ app/src/main/java/com/example/album4pro/gallery/DetailPhoto.java
@@ -2,22 +2,34 @@ package com.example.album4pro.gallery;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.content.ActivityNotFoundException;
+import android.content.ClipData;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.dsphotoeditor.sdk.activity.DsPhotoEditorActivity;
 import com.dsphotoeditor.sdk.utils.DsPhotoEditorConstants;
+import com.example.album4pro.BuildConfig;
 import com.example.album4pro.ImagesGallery;
 import com.example.album4pro.R;
 
 import java.io.File;
+import java.io.OutputStream;
 import java.util.List;
 
 public class DetailPhoto extends AppCompatActivity {
@@ -27,12 +39,15 @@ public class DetailPhoto extends AppCompatActivity {
     private ImageButton btnEdit;
     private ImageButton btnDelete;
     private ImageButton btnMore;
+    private Context mContext;
 
     private String pathImage = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_photo);
+
+        mContext = getApplicationContext();
 
         img = findViewById(R.id.img_detail);
         pathImage = getIntent().getStringExtra("path");
@@ -46,14 +61,32 @@ public class DetailPhoto extends AppCompatActivity {
         btnShare.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                BitmapDrawable bitmapDrawable = (BitmapDrawable) img.getDrawable();
+                Bitmap bitmap = bitmapDrawable.getBitmap();
 
+                ContentValues contentValues = new ContentValues();
+                contentValues.put(MediaStore.Images.Media.TITLE, "title");
+                contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+                Uri uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+
+                try {
+                    OutputStream outputStream = getContentResolver().openOutputStream(uri);
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+                    outputStream.close();
+
+                    Intent intent = new Intent(Intent.ACTION_SEND);
+                    intent.setType("image/jpeg");
+                    intent.putExtra(Intent.EXTRA_STREAM, uri);
+                    startActivity(Intent.createChooser(intent, "Share"));
+                } catch (Exception e){
+                    Log.e("ERROR SHARE", e.toString());
+                }
             }
         });
         btnEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Uri filePath = Uri.fromFile(new File(pathImage));
-                Log.e("URI nha", filePath.toString());
                 Intent dsPhotoEditorIntent = new Intent(DetailPhoto.this, DsPhotoEditorActivity.class);
                 dsPhotoEditorIntent.setData(filePath);
 
