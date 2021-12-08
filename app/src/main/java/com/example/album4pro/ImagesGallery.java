e397c120604c59df1802cf93958af4200533c2c5
Huong <hqc1909@gmail.com>
2021-12-09 03:11:28 +0700

    Gửi lại album, lần push trước nhần với drawable v24

--- app/src/main/java/com/example/album4pro/ImagesGallery.java
+++ app/src/main/java/com/example/album4pro/ImagesGallery.java
@@ -13,6 +13,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class ImagesGallery {
     public static ArrayList<String> listPhotoAndVideo(Context context){
@@ -166,5 +167,81 @@ public class ImagesGallery {
         Date stringDate = simpledateformat.parse(aDate, pos);
         return stringDate;
 
+    }
+    //=======================================================================================
+    public static ArrayList<String> listFolderName(Context context){
+        Uri uri;
+        Cursor cursor;
+        int column_index_data, column_index_folder_name;
+        ArrayList<String> listOfFolderImageName = new ArrayList<>();
+        String pathOfImageName;
+
+        uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        String[] projection = {
+                MediaStore.MediaColumns.DATA,
+                MediaStore.Images.Media.BUCKET_DISPLAY_NAME
+        };
+        String orderBy = MediaStore.Images.Media.DATE_TAKEN;
+        cursor = context.getContentResolver().query(uri, projection, null, null, orderBy + " DESC");
+        /* column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);*/
+
+        // get folder name
+        column_index_folder_name = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
+
+        while (cursor.moveToNext()){
+            pathOfImageName = cursor.getString(column_index_folder_name);
+            listOfFolderImageName.add(pathOfImageName);
+        }
+
+        //filter name folder
+        ArrayList<String> tempName = new ArrayList<>();
+        for (int index = 0; index < listOfFolderImageName.size(); index++){
+            if (!tempName.contains(listOfFolderImageName.get(index))){
+                tempName.add(listOfFolderImageName.get(index));
+            }
+        }
+        listOfFolderImageName.clear();
+        listOfFolderImageName.addAll(tempName);
+
+        return listOfFolderImageName;
+    }
+
+    public static ArrayList<String> listImageOnAlbum(Context context, String folderName){
+        Uri uri;
+        Cursor cursor;
+        int column_index_data, column_index_folder_name;
+        ArrayList<String> listImageOnAlbum = new ArrayList<>();
+        String pathOfImage, pathOfName;
+
+        uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        String[] projection = {
+                MediaStore.MediaColumns.DATA,
+                MediaStore.Images.Media.BUCKET_DISPLAY_NAME
+        };
+        String orderBy = MediaStore.Images.Media.DATE_TAKEN;
+        cursor = context.getContentResolver().query(uri, projection, null, null, orderBy + " DESC");
+        column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+        column_index_folder_name = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
+
+        while (cursor.moveToNext()){
+            pathOfImage = cursor.getString(column_index_data);
+            pathOfName = cursor.getString(column_index_folder_name);
+            if (folderName.equals(pathOfName)){
+                listImageOnAlbum.add(pathOfImage);
+            }
+        }
+        return listImageOnAlbum;
+    }
+
+    public static int firstImageOnAlbum(List<String> listImageOnALbum){
+        return Integer.parseInt(listImageOnALbum.get(0));
+    }
+
+    public static String numberImageOnAlbum(List<String> listImageOnAlbum){
+        int number = 0;
+        for (String imgOnALbum : listImageOnAlbum){
+            number++;
+        }
+        return Integer.toString(number);
     }
 }
