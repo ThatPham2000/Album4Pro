18a269ffe0205979089c2581ec1cec2bd0fa4a3e
NguyenCatTuong <nct542@gmail.com>
2021-12-29 18:43:14 +0700

    set 3 animation for SlideShow

--- app/src/main/java/com/example/album4pro/SlideShow.java
+++ app/src/main/java/com/example/album4pro/SlideShow.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 import com.bumptech.glide.Glide;
@@ -19,7 +21,7 @@ public class SlideShow extends AppCompatActivity {
     private ViewFlipper viewFlipper;
     private List<String> listPhoto;
     private Context context = null;
-    private Animation animEnter, animExit;
+    private Animation animEnter, animExit, animZoomIn, animZoomOut, animRotateIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +37,9 @@ public class SlideShow extends AppCompatActivity {
         // Hiệu ứng chuyển cảnh
         animEnter = AnimationUtils.loadAnimation(context, R.anim.anim_enter);
         animExit = AnimationUtils.loadAnimation(context, R.anim.anim_exit);
-
+        animZoomIn = AnimationUtils.loadAnimation(context, R.anim.zoom_in_slide);
+        animZoomOut = AnimationUtils.loadAnimation(context, R.anim.zoom_out_slide);
+        animRotateIn = AnimationUtils.loadAnimation(context, R.anim.anim_rotate_in);
 
         // Các cài đặt cho ViewFlipper
         for(int i = 0; i < listPhoto.size(); i++) {
@@ -45,9 +49,31 @@ public class SlideShow extends AppCompatActivity {
             //imgPhoto.setScaleType(ImageView.ScaleType.CENTER_CROP);
             viewFlipper.addView(imgPhoto);
         }
-        viewFlipper.setInAnimation(animEnter);
-        viewFlipper.setOutAnimation(animExit);
-        viewFlipper.setFlipInterval(2500); // Sau 0,25 giây thì chuyển hình ảnh
-        viewFlipper.setAutoStart(true);
+
+        Handler handler = new Handler();
+        final int[] i = {0};
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                switch (i[0] % 3){
+                    case 0:
+                        viewFlipper.setInAnimation(animEnter);
+                        viewFlipper.setOutAnimation(animExit);
+                        break;
+                    case 1:
+                        viewFlipper.setInAnimation(animRotateIn);
+                        viewFlipper.setOutAnimation(animZoomOut);
+                        break;
+                    case 2:
+                        viewFlipper.setInAnimation(animZoomIn);
+                        viewFlipper.setOutAnimation(animZoomOut);
+                        break;
+                }
+                i[0]++;
+                viewFlipper.showNext();
+                handler.postDelayed(this, 2500);
+            }
+        }, 2000);
+        //viewFlipper.setFlipInterval(2500); // Sau 0,25 giây thì chuyển hình ảnh
     }
 }
