9ae153ec251ef5c89fec68643bb9782ea506a0b4
PHẠM VĂN THẬT <57900109+ThatPham2000@users.noreply.github.com>
2022-01-13 10:26:22 +0700

    set theme

--- app/src/main/java/com/example/album4pro/albums/AlbumPage.java
+++ app/src/main/java/com/example/album4pro/albums/AlbumPage.java
@@ -63,6 +63,22 @@ public class AlbumPage extends AppCompatActivity implements View.OnClickListener
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set Theme Before SetContentView, Default Is Light Theme
+        sharedPreferences = getSharedPreferences("save", MODE_PRIVATE);
+
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_NO) {
+            if (sharedPreferences.getBoolean("smoke", false)) setTheme(R.style.SmokeTheme);
+            if (sharedPreferences.getBoolean("blue", true)) setTheme(R.style.Theme_Album4Pro);
+            if (sharedPreferences.getBoolean("brown", false)) setTheme(R.style.BrownTheme);
+            if (sharedPreferences.getBoolean("purple", false)) setTheme(R.style.PurpleTheme);
+            if (sharedPreferences.getBoolean("yellow", false)) setTheme(R.style.YellowTheme);
+            if (sharedPreferences.getBoolean("green", false)) setTheme(R.style.GreenTheme);
+            if (sharedPreferences.getBoolean("orange", false)) setTheme(R.style.OrangeTheme);
+            if (sharedPreferences.getBoolean("navy", false)) setTheme(R.style.NavyTheme);
+            if (sharedPreferences.getBoolean("pink", false)) setTheme(R.style.PinkTheme);
+        }
+
         setContentView(R.layout.activity_album_page);
         recyclerView = findViewById(R.id.rcv_page_album);
 
